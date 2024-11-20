8bdf6417b50d67b265a454562d9f4fc5d7eeee89
AnTruong100 <145039090+AnTruong100@users.noreply.github.com>
2024-11-20 23:47:11 +0700

    Add files via upload

---
+++ TanTienStore/Controllers/KhachHangsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TVA_KhachHang.Data;
+using TVA_KhachHang.Models;
+
+namespace TVA_KhachHang.Controllers
+{
+    public class KhachHangsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public KhachHangsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: KhachHangs
+        public async Task<IActionResult> Index()
+        {
+            
+            return View(await _context.KhachHang.ToListAsync());
+        }
+
+        // GET: KhachHangs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var khachHang = await _context.KhachHang
+                .FirstOrDefaultAsync(m => m.MaKH == id);
+            if (khachHang == null)
+            {
+                return NotFound();
+            }
+
+            return View(khachHang);
+        }
+
+        // GET: KhachHangs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: KhachHangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create( KhachHang khachHang)
+        {
+            
+                _context.Add(khachHang);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            
+            return View(khachHang);
+        }
+
+        // GET: KhachHangs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var khachHang = await _context.KhachHang.FindAsync(id);
+            if (khachHang == null)
+            {
+                return NotFound();
+            }
+            return View(khachHang);
+        }
+
+        // POST: KhachHangs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MaKH,Ho,TenDem,Ten,GioiTinh,DiaChi,SDT,DiemTichLuy")] KhachHang khachHang)
+        {
+            
+            if (id != khachHang.MaKH)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var existingCustomer = await _context.KhachHang.AsNoTracking().FirstOrDefaultAsync(kh => kh.MaKH == id);
+
+                    if (existingCustomer == null)
+                    {
+                        return NotFound();
+                    }
+
+                    // Giữ lại điểm tích lũy cũ (không thay đổi)
+                    khachHang.DiemTichLuy = existingCustomer.DiemTichLuy;
+
+                    // Cập nhật thông tin khách hàng
+                    _context.Entry(khachHang).State = EntityState.Modified;
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!KhachHangExists(khachHang.MaKH))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(khachHang);
+        }
+
+        // GET: KhachHangs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var khachHang = await _context.KhachHang
+                .FirstOrDefaultAsync(m => m.MaKH == id);
+            if (khachHang == null)
+            {
+                return NotFound();
+            }
+
+            return View(khachHang);
+        }
+
+        // POST: KhachHangs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var khachHang = await _context.KhachHang.FindAsync(id);
+            if (khachHang != null)
+            {
+                _context.KhachHang.Remove(khachHang);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool KhachHangExists(int id)
+        {
+            return _context.KhachHang.Any(e => e.MaKH == id);
+        }
+    }
+}
