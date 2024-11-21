6d3cb2c3ed3da011f4431704ab673fc1ac0f9167
AnTruong100 <145039090+AnTruong100@users.noreply.github.com>
2024-11-21 10:39:53 +0700

    Update KhachHangModel.cs

--- TanTienStore/Models/KhachHangModel.cs
+++ TanTienStore/Models/KhachHangModel.cs
@@ -1,7 +1,29 @@
-﻿namespace TanTienStore.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TanTienStore.Models
 {
     public class KhachHangModel
     {
-
+        [Key]
+        public int MaKH { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Ho { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(30)]
+        public string TenDem { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(20)]
+        public string Ten { get; set; } = string.Empty;
+        public bool GioiTinh { get; set; } = true;
+        [Required]
+        [MaxLength(255)]
+        public string? DiaChi { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string? SDT { get; set; }
+        public int DiemTichLuy { get; set; } = 0;
+        
     }
 }
